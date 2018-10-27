fa12f13c92f014b0ca9507f6e1eceb5b4d311f21
ezio84 <brabus84@gmail.com>
2018-10-27 17:23:15 +0200

    Set abc package name

--- src/com/abc/support/preferences/SystemSettingEditTextPreference.java
+++ src/com/abc/support/preferences/SystemSettingEditTextPreference.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.dirtyunicorns.support.preferences;
+package com.abc.support.preferences;
 
 import android.content.Context;
 import android.support.v7.preference.EditTextPreference;
